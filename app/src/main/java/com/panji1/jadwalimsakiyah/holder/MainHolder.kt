ec60e8beb1b48e089595643f14f5e274470fff7a
soaprush <dwipanji8@gmail.com>
2020-07-28 21:55:49 +0700

    jadwalimsakiyah1

---
+++ app/src/main/java/com/panji1/jadwalimsakiyah/holder/MainHolder.kt
@@ -0,0 +1,27 @@
+package com.panji.jadwalimsakiyah.holder
+
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.panji.jadwalimsakiyah.R
+
+
+
+class MainHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    @JvmField
+    var rlPrayList: RelativeLayout
+
+    @JvmField
+    var tvDateMasehi: TextView
+
+    @JvmField
+    var tvDateHijri: TextView
+
+    init {
+        rlPrayList = itemView.findViewById(R.id.rlPrayList)
+        tvDateMasehi = itemView.findViewById(R.id.tvDateMasehi)
+        tvDateHijri = itemView.findViewById(R.id.tvDateHijri)
+    }
+}
